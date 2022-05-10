94fa080c2c6e8858c3b8b23afcb65d51bf7c052a
Karim Aljandali <karimaljandali@gmail.com>
2022-05-10 17:22:11 -0400

    Added logo and sticky header on scroll

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { MenuIcon } from '@heroicons/react/solid'
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+  
+  const listenScrollEvent = () => {
+    window.scrollY > 10
+      ? setIsScrolled(true)
+      : setIsScrolled(false)
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent)
+  })
+
   return (
-    <div className="w-100 fixed top-0 inset-x-0">
-        <div className="mx-auto max-w-screen-xl">
+    <div className={`w-100 fixed top-0 inset-x-0 transition-all duration-200 z-10 ${isScrolled ? "bg-stone-700" : "bg-transparent" }`}>
+        <div className='mx-auto max-w-screen-xl'>
             <div className="px-2 py-8 flex justify-between items-center">
-                <a href="/" className="uppercase font-changa text-5xl text-white">KA</a>
-                <MenuIcon className="h-10 w-10 text-cyan-600 cursor-pointer" />
+                <a href="/" className="">
+                  <img src="/imgs/logo.png" alt="Karim Aljandali Logo" className="w-12 h-auto"/>
+                </a>
+                <MenuIcon className="h-10 w-10 text-pink-500 cursor-pointer" />
             </div>
         </div>
     </div>
